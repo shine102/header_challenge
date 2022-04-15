8df8ac436c3adb3d8fd575c55fd529cb89adc76c
shine102 <nguyenngotunglam2@gmail.com>
2022-04-15 15:09:59 +0700

    fix

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from flask import Flask, request, make_response, render_template
-import base64
 app = Flask('app')
 
 @app.route('/')
@@ -9,18 +8,19 @@ def hello_world():
 @app.route('/ehc', methods=["GET"])
 def welcome():
     ehc = 0
+    print(request.host)
     if request.user_agent.string.lower() == 'ehc':
+      print(request.host + "1")
       ehc = 1
-    if ehc == 1 and request.referrer == 'https://webchallenge.shine102.repl.co':
+    if ehc == 1 and request.referrer == 'http://' + request.remote_addr:
       ehc = 3
     if ehc == 3 and request.headers.get('DNT') == '1':
       ehc = 4
     if ehc == 4 and request.headers.get('Accept-Language') == 'ja':
       ehc = 5
     if ehc == 5 and request.headers.get('Date') == '2017':
-      print(1)
       ehc = 6
     return make_response(render_template('ehc.html', flag=ehc))
 
 
-app.run(host='0.0.0.0', port=8080)
+app.run(host='0.0.0.0', port=8090)
